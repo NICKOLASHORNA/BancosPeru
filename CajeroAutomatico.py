ce9627c5c025a6aa4777b88caea061d265c687b6
NICKOLASHORNA <nickohorna211011@gmail.com>
2026-07-15 15:35:43 -0500

    Update CajeroAutomatico.py
    codigo modificado para interbank
    autor: Nickolas Horna

--- CajeroAutomatico.py
+++ CajeroAutomatico.py
@@ -1,16 +1,16 @@
 # ===========================================
 # PROGRAMA: CAJERO AUTOMÁTICO
-# Autor: NICKOLAS HORNA IGNACIO
+# Autor: NICKOLAS HORNA IGNACIO 3sec
 # ===========================================
 
-saldo = 5000
-pin_correcto = "5678"
+saldo = 2000
+pin_correcto = "9012"
 
 print("===================================")
-print("     BIENVENIDO AL CAJERO DEL BCP")
+print("     CAJERO BANCO INTERBANK")
 print("===================================")
 
-pin = input("Ingrese su clave PIN: ")
+pin = input("Ingresar PIN: ")
 
 if pin == pin_correcto:
 
